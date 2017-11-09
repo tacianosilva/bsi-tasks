81f095de79210abf1d90124b3852b11694ba0f33
Taciano Morais Silva <tacianosilva@gmail.com>
2017-11-09 14:13:16 -0300

    Update esquema com foreign key

--- scripts_db/esquema_empresa_creates.sql
+++ scripts_db/esquema_empresa_creates.sql
@@ -1,26 +1,26 @@
-drop table if exists empregado cascade;
-drop table if exists departamento cascade;
-drop table if exists projeto cascade;
-drop table if exists alocacao cascade;
 drop table if exists dependente cascade;
+drop table if exists alocacao cascade;
+drop table if exists projeto cascade;
+drop table if exists departamento cascade;
+drop table if exists empregado cascade;
 
 CREATE TABLE `empregado` (
   `nome` varchar(15) NOT NULL,
-  `matricula` char(9) NOT NULL,
+  `matricula` int(11) NOT NULL,
   `dataNasc` date DEFAULT NULL,
   `endereco` varchar(30) DEFAULT NULL,
   `sexo` char(1) DEFAULT NULL,
   `salario` decimal(10,2) DEFAULT NULL,
-  `supervisor` char(9) DEFAULT NULL,
+  `supervisor` int(11) DEFAULT NULL,
   `depto` int(11) DEFAULT NULL,
   PRIMARY KEY (`matricula`),
-  CONSTRAINT `empSuperFK` FOREIGN KEY (`supervisor`) REFERENCES `empregado` (`matricula`)
+  CONSTRAINT `empSuperFK` FOREIGN KEY (`supervisor`) REFERENCES `empregado` (`matricula`) on delete set null
 );
 
 CREATE TABLE `departamento` (
   `nomeDep` varchar(15) NOT NULL,
   `codDep` int(11) NOT NULL,
-  `gerente` char(9) NOT NULL,
+  `gerente` int(11) NOT NULL,
   `dataInicioGer` date DEFAULT NULL,
   PRIMARY KEY (`codDep`),
   UNIQUE KEY `nomeDep` (`nomeDep`)
@@ -30,19 +30,17 @@ CREATE TABLE `projeto` (
   `codproj` int(11) NOT NULL,
   `nome` varchar(45) DEFAULT NULL,
   `local` varchar(45) DEFAULT NULL,
-  `depart` varchar(45) DEFAULT NULL,
+  `depart` int(11) DEFAULT NULL,
   `projetocol` int(11) DEFAULT NULL,
   PRIMARY KEY (`codproj`),
-  foreign key (depart) references departamento(codDep)
+  foreign key (depart) references departamento(codDep) on delete set null
 );
 
 CREATE TABLE `alocacao` (
   `matric` int(11) NOT NULL,
   `codigop` int(11) NOT NULL,
   `horas` decimal(4,2) DEFAULT NULL,
-  PRIMARY KEY (`matric`,`codigop`),
-  foreign key (matric) references empregado(matricula),
-  foreign key (codigop) references projeto(codproj)
+  PRIMARY KEY (`matric`,`codigop`)
 );
 
 CREATE TABLE `dependente` (
@@ -50,9 +48,12 @@ CREATE TABLE `dependente` (
   `matricula` int(11) NOT NULL,
   `nome` varchar(45) DEFAULT NULL,
   `sexo` varchar(1) DEFAULT NULL,
-  PRIMARY KEY (`coddepend`,`matricula`),
-  foreign key (matricula) references empregado(matricula)
+  PRIMARY KEY (`coddepend`,`matricula`)
 );
+
+alter table alocacao add foreign key (`matric`) references empregado(matricula) on delete restrict;
+alter table alocacao add foreign key (`codigop`) references projeto(codproj) on delete restrict;
+alter table dependente add foreign key (matricula) references empregado(matricula) on delete restrict;
 
 # Carga inicial
 
@@ -62,7 +63,7 @@ values (9491, 'Ana');
 
 insert into empregado
 (matricula, nome)
-values (9491, 'Taciano');
+values (9492, 'Taciano');
 
 insert into departamento 
 (codDep, nomeDep, gerente)
@@ -76,4 +77,4 @@ update empregado set depto = 1 where matricula = '9491';
 update empregado set depto = 1 where matricula = '9492';
 
 alter table empregado add constraint empDepFK
-foreign key (depto) references departamento(codDep);
+foreign key (depto) references departamento(codDep) on delete set null;
