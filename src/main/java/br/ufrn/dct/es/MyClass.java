9ca78f2ac0b207a5b58e981e4a8e8858e685281a
Taciano Morais Silva <tacianosilva@gmail.com>
2019-05-02 15:00:44 -0300

    Adição exemplos de testes e libs para o JUnit e TestNG.

---
+++ src/main/java/br/ufrn/dct/es/MyClass.java
@@ -0,0 +1,9 @@
+package br.ufrn.dct.es;
+
+public class MyClass {
+
+    public Integer multiply(int i, int j) {
+        return i * j;
+    }
+
+}
---
+++ src/test/java/br/ufrn/dct/es/MyClassTestNG.java
@@ -0,0 +1,18 @@
+package br.ufrn.dct.es;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class MyClassTestNG {
+
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+        MyClass tester = new MyClass(); // MyClass is tested
+
+        // assert statements
+        assertEquals(0, tester.multiply(10, 0), "10 x 0 must be 0");
+        assertEquals(0, tester.multiply(0, 10), "0 x 10 must be 0");
+        assertEquals(0, tester.multiply(0, 0), "0 x 0 must be 0");
+    }
+}
