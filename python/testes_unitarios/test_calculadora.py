e3ba5c9de33e130e6fab2a03acfcc48f33e1b518
Taciano Morais Silva <tacianosilva@gmail.com>
2021-02-08 21:33:52 -0300

    Add teste de unidade da calculadora (unittest)

---
+++ python/testes_unitarios/test_calculadora.py
@@ -0,0 +1,23 @@
+"""
+    Testes de Unidade da calculadora.py.
+"""
+import unittest
+
+import calculadora
+
+class CalculadoraTest(unittest.TestCase):
+    """
+        Classe de Teste da calculadora.
+
+    Args:
+        unittest ([TestCase]): Testa as funções da calculadora.
+    """
+
+    def test(self):
+        """
+            Testa a função de soma para dois números.
+        """
+        self.assertEqual(calculadora.soma(3, 5), 8)
+
+if __name__ == '__main__':
+    unittest.main()
