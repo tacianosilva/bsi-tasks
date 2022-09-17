58bba7861dbe85fc4f2e707d376d21253480e4b2
GuilhermeAngelo <guilhermeangelo2001@gmail.com>
2022-09-17 12:43:13 -0300

    #22 retirando o crud do repositorio

--- softwaretesting/20222/GuilhermeAngelo/crud/app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-def home(request):
-    return render(request, 'index.html')
-
-def form(request):
-    return render(request, "form.html")
