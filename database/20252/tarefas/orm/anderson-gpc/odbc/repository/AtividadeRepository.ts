544f1fcdb1b3bfe563860762eaa2f21083179b44
andersonstack <andersong.pereiracruz@gmail.com>
2025-10-15 18:37:53 -0300

    feat: método que traz todas as atividades e projetos #197

---
+++ database/20252/tarefas/orm/anderson-gpc/odbc/repository/AtividadeRepository.ts
@@ -0,0 +1,28 @@
+import conexaoODBC from "../service/server";
+import odbc from "odbc";
+
+export class AtividadeRepository {
+    async pegarTodasAtividades() {
+        let conexao: odbc.Connection | null = null;
+        try {
+            conexao = await conexaoODBC();
+            const resultado = await conexao.query(`SELECT 
+                a.codigo,
+                a.descricao AS atividade_descricao,
+                a.projeto AS projeto_codigo,
+                a.data_inicio,
+                a.data_fim,
+                p.nome AS projeto_nome,
+                p.descricao AS projeto_descricao
+                FROM atividade a
+                JOIN projeto p ON p.codigo = a.projeto;`
+            );
+            return resultado.filter((key) => key != 'columns');
+        } catch (error) {
+            console.error("Erro de conexão ou query:", error);
+            return [];
+        } finally {
+            if (conexao) await conexao.close();
+        }
+    }
+}
