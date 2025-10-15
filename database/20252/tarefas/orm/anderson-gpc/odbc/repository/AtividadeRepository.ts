71f3d36ba35d542dcf897f598fb81ee1622d4229
andersonstack <andersong.pereiracruz@gmail.com>
2025-10-15 19:55:37 -0300

    feat: criando método de adicionar nova tarefa #197

--- database/20252/tarefas/orm/anderson-gpc/odbc/repository/AtividadeRepository.ts
+++ database/20252/tarefas/orm/anderson-gpc/odbc/repository/AtividadeRepository.ts
@@ -1,4 +1,6 @@
+import { atividadeSchema } from "../schema/atividade.shcema";
 import conexaoODBC from "../service/server";
+import ProjetoRepository from "./ProjetoRepository";
 
 class AtividadeRepository {
     private conexao: any | null;
@@ -15,9 +17,8 @@ class AtividadeRepository {
                 p.nome AS projeto_nome,
                 p.descricao AS projeto_descricao
                 FROM atividade a
-                JOIN projeto p ON p.codigo = a.projeto;`
-            );
-            return resultado.filter((key: any) => key != 'columns');
+                JOIN projeto p ON p.codigo = a.projeto;`);
+            return resultado.filter((key: any) => key != "columns");
         } catch (error) {
             console.error("Erro de conexão ou query:", error);
             return [];
@@ -26,8 +27,44 @@ class AtividadeRepository {
         }
     }
 
-    async criarAtividade() {
+    async criarAtividade(data: any) {
+        const parsed = atividadeSchema.safeParse(data);
+        if (!parsed.success) {
+            return { success: false, message: "Dados inválidos." };
+        }
 
+        const { descricao, projeto, data_fim, data_inicio } = parsed.data;
+        this.conexao = await conexaoODBC();
+
+        try {
+            let codProjeto: number | null = null;
+
+            if (projeto !== undefined && projeto !== null) {
+                const projetoExiste = await this.projetoExiste(projeto);
+                if (!projetoExiste) {
+                    throw new Error("Projeto informado não existe.");
+                }
+                codProjeto = projeto;
+            }
+
+            await this.conexao.query(
+                `INSERT INTO atividade (descricao, projeto, data_fim, data_inicio)
+                 VALUES (?, ?, ?, ?)`,
+                [descricao, codProjeto, data_fim, data_inicio]
+            );
+
+            return { success: true, message: "Atividade criada com sucesso!" };
+        } catch (error: any) {
+            console.error("Erro de conexão ou query:", error);
+            return { success: false, message: error.message };
+        } finally {
+            if (this.conexao) await this.conexao.close();
+        }
+    }
+
+    async projetoExiste(projeto: number): Promise<boolean> {
+        const codProjeto = await ProjetoRepository.projetoExiste(projeto);
+        return !!codProjeto;
     }
 }
 
